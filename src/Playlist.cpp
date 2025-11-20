9e8d6152a38daf0179772b110a1e8ee24c49c017
BD-Ali <Badarne3li@gmail.com>
2025-11-20 12:00:31 +0000

    phase 1, playlist memory leaks fixed

--- src/Playlist.cpp
+++ src/Playlist.cpp
@@ -12,6 +12,13 @@ Playlist::~Playlist() {
     #ifdef DEBUG
     std::cout << "Destroying playlist: " << playlist_name << std::endl;
     #endif
+    PlaylistNode* current = head;
+    while (current) {
+        PlaylistNode* to_delete = current;
+        current = current->next;
+        delete to_delete;
+    }
+    track_count = 0;
 }
 
 void Playlist::add_track(AudioTrack* track) {
@@ -49,6 +56,7 @@ void Playlist::remove_track(const std::string& title) {
         } else {
             head = current->next;
         }
+        delete current;  // Free the node memory
 
         track_count--;
         std::cout << "Removed '" << title << "' from playlist" << std::endl;
